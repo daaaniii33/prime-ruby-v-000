a5de1ff541f59385b656c09b9373e5c32757cef5
daaaniii33 <daaaniii33@gmail.com>
2019-01-16 18:49:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,9 @@
-def prime?(num)
-  (2..number-1).each {|n| return false if number <= 1 || number % n == 0}
- return true
+def prime?(integer)
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
+    end
+    true
 end
